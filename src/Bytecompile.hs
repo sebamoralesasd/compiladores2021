d0449278e82aba712756dcb5a94fe691fdb6ced2
sebamoralesasd <scmsasd@gmail.com>
2021-10-25 15:31:55 -0300

    Add: primera integración con main con comentarios varios. Arreglar

--- src/Bytecompile.hs
+++ src/Bytecompile.hs
@@ -160,6 +160,7 @@ bytecompileModule mdl =
       declsToTerm :: MonadFD4 m => [Decl Term] -> m Term
       -- TODO: Cambiar el tipo Nat acordemente
       declsToTerm [] = undefined 
+      -- TODO: recorrer t para los Global
       declsToTerm [Decl pos name t] = return $ Let pos name NatTy t (V pos (Free name))
       declsToTerm ((Decl pos name t): k) =
         do 
