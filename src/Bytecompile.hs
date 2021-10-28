c80a76d7e72c3d440f86a3be92b2c31585fc40d7
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2021-10-28 14:22:43 -0300

    Step implementation and prints

--- src/Bytecompile.hs
+++ src/Bytecompile.hs
@@ -15,11 +15,12 @@ module Bytecompile (Bytecode, runBC, bcWrite, bcRead, bytecompileModule, humanRe
 import Data.Binary (Binary (get, put), Word32, decode, encode)
 import Data.Binary.Get (getWord32le, isEmpty)
 import Data.Binary.Put (putWord32le)
-import Data.Char (ord)
 import qualified Data.ByteString.Lazy as BS
+import Data.Char (ord)
 import Lang
 import MonadFD4
 import Subst (close)
+import PPrint (pp)
 
 type Opcode = Int
 
@@ -32,6 +33,7 @@ type Stack = [Val]
 type Env = [Val]
 
 data Val = I Int | Fun Env Bytecode | RA Env Bytecode
+  deriving Show
 
 instance Num (Val) where
   (I x) + (I y) = I $ x + y
@@ -152,21 +154,23 @@ bc (Let i name ty term1 term2) =
 type Module = [Decl Term]
 
 bytecompileModule :: MonadFD4 m => Module -> m Bytecode
-bytecompileModule mdl = 
-  do 
+bytecompileModule mdl =
+  do
     term <- declsToTerm mdl
+    ppTerm <- pp term
+    printFD4 ppTerm
     termByteCode <- bc term
     return $ termByteCode ++ [PRINTN] ++ [STOP]
-    where
-      declsToTerm :: MonadFD4 m => [Decl Term] -> m Term
-      -- TODO: Cambiar el tipo Nat acordemente
-      declsToTerm [] = undefined 
-      -- TODO: recorrer t para los Global
-      declsToTerm [Decl pos name t] = return $ close name $ Let pos name NatTy t (V pos (Free name))
-      declsToTerm ((Decl pos name t): k) =
-        do 
-          kt <- declsToTerm k
-          return $ Let pos name NatTy t kt
+  where
+    declsToTerm :: MonadFD4 m => [Decl Term] -> m Term
+    -- TODO: Cambiar el tipo Nat acordemente
+    declsToTerm [] = undefined
+    -- TODO: recorrer t para los Global
+    declsToTerm [Decl pos name t] = return $ close name $ Let pos name NatTy t (V pos (Free name))
+    declsToTerm ((Decl pos name t) : k) =
+      do
+        kt <- declsToTerm k
+        return $ Let pos name NatTy t kt
 
 -- | Toma un bytecode, lo codifica y lo escribe un archivo
 bcWrite :: Bytecode -> FilePath -> IO ()
@@ -185,60 +189,14 @@ bcRead filename = (map fromIntegral <$> un32) . decode <$> BS.readFile filename
 runBC :: MonadFD4 m => Bytecode -> m ()
 runBC byteCode = runBC' byteCode [] []
 
-
-
 runBC' :: MonadFD4 m => Bytecode -> Env -> Stack -> m ()
-runBC' (NULL : c) e s = undefined
-runBC' (RETURN : _) _ (v : RA e c : s) =
-  runBC' c e (v : s)
-runBC' (CONST : n : c) e s =
-  runBC' c e (I n : s)
-runBC' (ACCESS : i : c) e s =
-  runBC' c e (e !! i : s)
-runBC' (FUNCTION : len : c) e s =
-  let c_f = take len c in runBC' c e (Fun e c_f : s)  
-runBC' (CALL : c) e (v : Fun e_f c_f : s) =
-  runBC' c_f (v : e_f) (RA e c : s)
-runBC' (ADD : c) e (n : m : s) =
-  runBC' c e (m + n : s)
-runBC' (SUB : c) e (n : m : s) =
-  runBC' c e (m - n : s)
-runBC' (POP_JUMP_IF_NOT_0 : len: c) e (I n: s) = 
-  if n == 0 
-    then runBC' c e s
-    else runBC' (drop len c) e s
-runBC' (FIX : c) e (Fun e_fix c_f: s) = 
-  runBC' c e s
-  where
-    -- TODO: revisar si efectivamente genera el nudo o el shadowing interfiere
-    e_fix = Fun e_fix c_f : e
-runBC' (STOP : c) e s = return ()
-runBC' (SHIFT : c) e (v : s) =
-  runBC' c (v : e) s
-runBC' (DROP : c) (v : e) s =
-  runBC' c e s
-runBC' (PRINT : c') e s =
+runBC' c e s =
   do
-    c <- consume c'
-    runBC' c e s
-    where
-      consume :: MonadFD4 m => Bytecode -> m Bytecode
-      consume (NULL: c) = return c
-      consume (x_i: c) = 
-        do 
-          printFD4 $ show x_i
-          consume c
-      consume _ = undefined 
-
-runBC' (PRINTN : c) e (I n : s) =
-  do
-    printFD4 $ show n
-    runBC' c e (I n : s)
-runBC' (JUMP : n : c) e s =
-  runBC' (drop n c) e s
-runBC' (command : c) e s = undefined
-runBC' [] _ _ = failFD4 "Interrupción inesperada: no hay más bytecode pero no se consumió STOP"
-
+    printFD4 $ show (humanReadableBC c, e, s)
+    nextStep <- runBCstep (c, e, s)
+    case nextStep of
+      ([], _, _) -> return ()
+      (c', e', s') -> runBC' c' e' s'
 
 -- Aux
 humanReadableOpcode :: Opcode -> String
@@ -263,58 +221,54 @@ humanReadableOpcode n = "Literal " ++ show n
 humanReadableBC :: Bytecode -> [String]
 humanReadableBC = map humanReadableOpcode
 
-
-
-runBCstep' :: MonadFD4 m => (Bytecode, Env, Stack) -> m (Bytecode, Env, Stack)
-runBCstep' ((NULL : c), e, s) = undefined
-runBCstep' ((RETURN : _), _, (v : RA e c : s)) =
-  return (c, e, (v : s))
--- runBCstep' (CONST : n : c) e s =
---   runBCstep' c e (I n : s)
--- runBCstep' (ACCESS : i : c) e s =
---   runBCstep' c e (e !! i : s)
--- runBCstep' (FUNCTION : len : c) e s =
---   let c_f = take len c in runBCstep' c e (Fun e c_f : s)  
--- runBCstep' (CALL : c) e (v : Fun e_f c_f : s) =
---   runBCstep' c_f (v : e_f) (RA e c : s)
--- runBCstep' (ADD : c) e (n : m : s) =
---   runBCstep' c e (m + n : s)
--- runBCstep' (SUB : c) e (n : m : s) =
---   runBCstep' c e (m - n : s)
--- runBCstep' (POP_JUMP_IF_NOT_0 : len: c) e (I n: s) = 
---   if n == 0 
---     then runBCstep' c e s
---     else runBCstep' (drop len c) e s
--- runBCstep' (FIX : c) e (Fun e_fix c_f: s) = 
---   runBCstep' c e s
---   where
---     -- TODO: revisar si efectivamente genera el nudo o el shadowing interfiere
---     e_fix = Fun e_fix c_f : e
--- runBCstep' (STOP : c) e s = return ()
--- runBCstep' (SHIFT : c) e (v : s) =
---   runBCstep' c (v : e) s
--- runBCstep' (DROP : c) (v : e) s =
---   runBCstep' c e s
--- runBCstep' (PRINT : c') e s =
---   do
---     c <- consume c'
---     runBCstep' c e s
---     where
---       consume :: MonadFD4 m => Bytecode -> m Bytecode
---       consume (NULL: c) = return c
---       consume (x_i: c) = 
---         do 
---           printFD4 $ show x_i
---           consume c
---       consume _ = undefined 
-
--- runBCstep' (PRINTN : c) e (I n : s) =
---   do
---     printFD4 $ show n
---     runBCstep' c e (I n : s)
--- runBCstep' (JUMP : n : c) e s =
---   runBCstep' (drop n c) e s
--- runBCstep' (command : c) e s = undefined
--- runBCstep' [] _ _ = failFD4 "Interrupción inesperada: no hay más bytecode pero no se consumió STOP"
-
-runBCstep' x = undefined
+-- La MonadFD4 es solo para printear
+runBCstep :: MonadFD4 m => (Bytecode, Env, Stack) -> m (Bytecode, Env, Stack)
+runBCstep (NULL : c, e, s) = undefined
+runBCstep (RETURN : _, _, v : RA e c : s) =
+  return (c, e, v : s)
+runBCstep (CONST : n : c, e, s) =
+  return (c, e, I n : s)
+runBCstep (ACCESS : i : c, e, s) =
+  return (c, e, e !! i : s)
+runBCstep (FUNCTION : len : c, e, s) =
+  let c_f = take len c in return (c, e, Fun e c_f : s)
+runBCstep (CALL : c, e, v : Fun e_f c_f : s) =
+  return (c_f, v : e_f, RA e c : s)
+runBCstep (ADD : c, e, n : m : s) =
+  return (c, e, m + n : s)
+runBCstep (SUB : c, e, n : m : s) =
+  return (c, e, m - n : s)
+runBCstep (POP_JUMP_IF_NOT_0 : len : c, e, I n : s) =
+  if n == 0
+    then return (c, e, s)
+    else return (drop len c, e, s)
+runBCstep (FIX : c, e, Fun e_fix c_f : s) =
+  return (c, e, s)
+  where
+    -- TODO: revisar si efectivamente genera el nudo o el shadowing interfiere
+    e_fix = Fun e_fix c_f : e
+runBCstep ([STOP], e, s) = return ([], e, s)
+runBCstep (SHIFT : c, e, v : s) =
+  return (c, v : e, s)
+runBCstep (DROP : c, v : e, s) =
+  return (c, e, s)
+runBCstep (PRINT : c', e, s) =
+  do
+    c <- consume c'
+    return (c, e, s)
+  where
+    consume :: MonadFD4 m => Bytecode -> m Bytecode
+    consume (NULL : c) = return c
+    consume (x_i : c) =
+      do
+        printFD4 $ show x_i
+        consume c
+    consume _ = undefined
+runBCstep (PRINTN : c, e, I n : s) =
+  do
+    printFD4 $ show n
+    return (c, e, I n : s)
+runBCstep (JUMP : n : c, e, s) =
+  return (drop n c, e, s)
+runBCstep ([], _, _) = failFD4 "Interrupción inesperada: no hay más bytecode pero no se consumió STOP"
+runBCstep (command : c, e, s) = undefined
