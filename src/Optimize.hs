d3bf4e2be4b2cf9db09a6cd139488b042dd74ae7
Agustin Ramiro Diaz <agustin.ramiro.diaz@gmail.com>
2021-11-16 18:10:34 -0300

    More!

--- src/Optimize.hs
+++ src/Optimize.hs
@@ -1,8 +1,10 @@
 module Optimize (optimizeTerm) where
-import Lang (Term, Tm (BinaryOp, Const, IfZ), Const (CNat), BinaryOp (Add, Sub))
+import Lang (Term, Tm (BinaryOp, Const, IfZ, Let), Const (CNat), BinaryOp (Add, Sub))
 import Eval (semOp)
+import Subst (subst)
 
 optimizeTerm :: Term -> Term
+{- Constant Folding -}
 optimizeTerm (BinaryOp i binaryOp (Const _ (CNat x)) (Const _ (CNat y))) =
   Const i (CNat (semOp binaryOp x y))
 -- x +- 0 = x
@@ -12,7 +14,12 @@ optimizeTerm (BinaryOp i Add (Const _ (CNat 0)) x) = x
 -- 0 - x = 0. TODO: acomodar printing y revisar divergencia
 optimizeTerm (BinaryOp i Sub (Const _ (CNat 0)) x) = Const i (CNat 0)
 
-optimizeTerm (IfZ i (Const _ (CNat 0)) term _) = term
-optimizeTerm (IfZ i (Const _ (CNat _)) _ term) = term
+optimizeTerm (IfZ _ (Const _ (CNat 0)) thenTerm _) = thenTerm
+optimizeTerm (IfZ _ (Const _ (CNat _)) _ elseTerm) = elseTerm
 
+{- Constant Propagation -}
+optimizeTerm (Let i name ty x@(Const _ (CNat _)) term) =
+    subst x term
+
+{- Not optimized -}
 optimizeTerm t = t
